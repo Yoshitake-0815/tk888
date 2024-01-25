54807b2c5cd51f906b7e8f16d8d60b9da994eff2
“Yoshitake” <kwzystk.0815@gmail.com>
2024-01-25 21:09:05 +0900

    ちかは酒豪3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   resources :users, only: [:show]
 
   resources :shindans
@@ -9,9 +8,6 @@ Rails.application.routes.draw do
   resources :tweets do
     resources :likes, only: [:create, :destroy]
   end
-  
-  
-  get "users" => "users#show"
 
   get 'manhattans' => 'manhattans#manhattan'
   get 'brons' => 'brons#bron'
